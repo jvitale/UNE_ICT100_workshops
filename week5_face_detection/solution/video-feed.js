edda2ca9d2c9573dd7251d975fd2baf47ddb89bc
Jonathan Vitale <vitale.jonathan@ymail.com>
2020-08-03 09:59:41 +1000

    Add solution to week 5 workshop

---
+++ week5_face_detection/solution/video-feed.js
@@ -0,0 +1,21 @@
+// getting video media element
+const webcam = document.getElementById('webcam-feed');
+
+async function openWebcam(constraints){
+    var stream = null;
+    try {
+        stream = await navigator.mediaDevices.getUserMedia(constraints);
+        webcam.srcObject = stream;
+    } catch(err) {
+        console.error('Error during opening webcam: ' + err);
+    }
+}
+
+// create a publisher to communicate when the webcam is ready
+webcam.addEventListener(
+    'play',
+    function (){
+        console.log('Webcam feed is now open');
+        manager.publish('webcam_opened', {videoInput: webcam});
+    }
+)
