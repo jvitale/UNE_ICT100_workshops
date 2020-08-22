7b9a0d11ca11c7871d4d03b9a995b5869bed1e3a
Jonathan Vitale <vitale.jonathan@ymail.com>
2020-08-22 18:26:21 +1000

    Add week9 code

---
+++ week9_nlp/material/face-detection.js
@@ -0,0 +1,147 @@
+// creating global variables to store
+// the video element and canvas for overlay
+var videoInput = null;
+var faceDetectorCanvas = null;
+var canvasDisplaySize = null;
+
+// setting the parameters for the webcam
+const webcamConstraints = {
+    audio: false,
+    video: {width: 320, height:240}
+}
+
+// creating a subscriber to intercept
+// the message sent when the webcam is
+// open and ready to stream
+let subwebcam = manager.subscribe(
+    'webcam_opened',
+    function (message){
+        // we save the video element in
+        // the global variable videoInput
+        videoInput = message.videoInput;
+
+        // we create a canvas as overlay of the video element
+        faceDetectorCanvas = faceapi.createCanvasFromMedia(videoInput);
+        let videoFrame = document.getElementById('video-frame');
+        videoFrame.append(faceDetectorCanvas);
+
+        // we set some properties to make sure that
+        // the canvas and the video elements
+        // are perfectly overlapping
+        faceDetectorCanvas.style.position = 'absolute';
+        faceDetectorCanvas.style.top = 0;
+        faceDetectorCanvas.style.left = 0;
+
+        canvasDisplaySize = {width: videoInput.width, height: videoInput.height};
+        faceapi.matchDimensions(faceDetectorCanvas, canvasDisplaySize);
+
+        // this subscriber will run a detection
+        // everytime a new message is sent
+        // for the topic 'detect_face'
+        manager.subscribe(
+            'detect_face', 
+            (msg) => detectFace(new faceapi.TinyFaceDetectorOptions())
+        );
+        
+        // this subscriber will draw the bounding
+        // boxes each time that a new message
+        // is sent for the topic 'face_detection_complete'
+        manager.subscribe(
+            'face_detection_complete',
+            manageFaceDetection
+        );
+        
+        // let's start the face detection with
+        // a first message on the topic
+        // 'detect_face'
+        manager.publish('detect_face', {});
+    }
+)
+
+// preload the model for the machine learning
+// model and open the webcam once loaded
+Promise.all([
+    console.log('Going to load the model'),
+    faceapi.nets.tinyFaceDetector.loadFromUri('models'),
+    faceapi.nets.faceLandmark68Net.loadFromUri('models'),
+    faceapi.nets.faceRecognitionNet.loadFromUri('models'),
+    faceapi.nets.ageGenderNet.loadFromUri('models'), 
+    console.log('Model loaded')
+]).then(openWebcam(webcamConstraints));
+
+// function to draw the bounding boxes
+// for the detected faces
+function manageFaceDetection(detectionMsg){
+    // getting the detections from the message
+    let detections = detectionMsg.detections;
+
+    // resize the bounding boxes of the detected faces
+    // accordingly to the size of the canvas
+    let resizedDetections = faceapi.resizeResults(detections, canvasDisplaySize);
+    
+    // cleaning the canvas from previous bounding boxes
+    faceDetectorCanvas.getContext('2d').clearRect(0, 0, faceDetectorCanvas.width, faceDetectorCanvas.height);
+    
+    // draw the new bounding boxes
+    faceapi.draw.drawDetections(faceDetectorCanvas, resizedDetections);
+
+    // publish a new empty message for the
+    // 'detect_face' topic, so that a new detection
+    // can be done
+    manager.publish('detect_face', {});
+}
+
+
+// function to detect the faces
+function detectFace(options){
+    // storing the current timestamp
+    // at the beginning of the detection
+    let startTimestamp = Date.now();
+
+    // detect the faces with faceapi
+    // !! this call is a Promise !!
+    // this means that it does not return
+    // the detections, but a Promise object that
+    // can be then used to check when the
+    // detection algorithm has completed
+    // and get the detected faces
+    faceapi.detectAllFaces(
+        videoInput,
+        options
+    ).then(
+        // this is invoke once the Promise
+        // created by detectAllFaces
+        // terminate with success
+        function (detections){
+            // we save the timestamp now
+            // after the end of the detection
+            let endTimestamp = Date.now();
+
+            // we compute the time difference in seconds
+            let timeElapsed = (endTimestamp - startTimestamp)/1000;
+
+            // we log the elapsed time
+            console.log('Detection took ' + timeElapsed + ' seconds');
+            
+            
+            // finally we publish a message
+            // for the topic 'face_detection_complete'
+            // and pass the detections as the message
+            manager.publish(
+                'face_detection_complete',
+                {detections: detections}
+            );
+        }
+    ).catch(
+        // this is executed only if the
+        // Promise created by detectAllFaces
+        // has some error
+        function (err){
+            console.error('Error during face detection: ' + err);
+        }
+    )
+}
+
+
+
+
---
+++ week9_nlp/video/chatbots.js
@@ -0,0 +1,372 @@
+// function to read from the input text boxes
+function readInputText(botName, clearInput){
+    let inputTextObj = document.getElementById('input-text-' + botName);
+    let text = inputTextObj.value;
+    if (clearInput == true){
+        inputTextObj.value = '';
+    }
+    return text;
+}
+
+// function to publish a chat message
+function publishChatMessage(botName, msgContent){
+    let message = {
+        bot: botName,
+        content: msgContent
+    };
+    manager.publish('user_message', message);
+}
+
+// function to show the text balloon
+function showTextBalloon(botName, message){
+    let balloon = document.getElementById('text-balloon-' + botName);
+    balloon.getElementsByClassName('bot-message')[0].textContent = message;
+    balloon.style.display = 'block';
+}
+
+// function to hide the text balloon
+function hideTextBalloon(botName){
+    let balloon = document.getElementById('text-balloon-' + botName);
+    balloon.getElementsByClassName('bot-message')[0].textContent = '';
+    balloon.style.display = 'none';
+}
+
+// declaring constants to get access to the buttons controlling
+// the chat
+const btnTextPico = document.getElementById('text-pico');
+const btnTextBit = document.getElementById('text-bit');
+const btnTalkPico = document.getElementById('talk-to-pico');
+const btnTalkBit = document.getElementById('talk-to-bit');
+
+// adding 'onclick' events to the buttons
+btnTextPico.addEventListener(
+    'click',
+    function() {
+        let text = readInputText('pico', true);
+        if (text.length > 0){
+            publishChatMessage('pico', text);
+        }
+    }
+)
+
+btnTextBit.addEventListener(
+    'click',
+    function() {
+        let text = readInputText('bit', true);
+        if (text.length > 0){
+            publishChatMessage('bit', text);
+        }
+    }
+)
+
+btnTalkPico.addEventListener(
+    'click',
+    function() {
+        listen(
+            function(result){
+                publishChatMessage('pico', result);
+            }
+        );
+    }
+)
+
+btnTalkBit.addEventListener(
+    'click',
+    function() {
+        listen(
+            function(result){
+                publishChatMessage('bit', result);
+            }
+        );
+    }
+)
+
+// function to change the emotion of the bot
+// when getting "I love you"
+async function manageEmotionalState(msg){
+    console.log('call to manageEmotionalState');
+    // pico gets happy
+    let emotionMessage = {
+        bot: msg.bot,
+        emotionalState: 'happy'
+    };
+    
+    let botSpeech = 'I love you too';
+    await speak(msg.bot, botSpeech);
+    manager.publish('emotional_state_changed', emotionMessage);
+}
+
+function retrieveName(answer){
+    let docx = nlp(answer);
+    let people = docx.match('#Person').json();
+
+    if (people.length > 0){
+        return people[0].text;
+    } else {
+        return '';
+    }
+}
+
+function isAnswerYes(answer){
+    let docx = nlp(answer);
+    return docx.has('(yes|sure|affermative|correct|definitely|obviously|true)');
+}
+
+async function askName(bot, maxAttempts){
+    let userName = '';
+    let nAttempts = 0;
+    while (userName === '' && nAttempts < maxAttempts){
+        let promiseListen = new Promise(
+            (resolve, reject) => {
+                listen((result) => resolve(result));
+            }
+        ).then((answer) => userName = retrieveName(answer));
+        await promiseListen;
+        nAttempts += 1;
+        if (userName === ''){
+            if (nAttempts < maxAttempts){
+                await speak(bot, 'Sorry, I can\'t get your name. What is your name?');
+            } else {
+                return '';
+            }
+        } else {
+            await speak(bot, 'You said that your name is ' + userName + '?');
+            let isCorrect = false;
+            let promiseCheck = new Promise(
+                (resolve, reject) => {
+                    listen((result) => resolve(result));
+                }
+            ).then((answer) => isCorrect = isAnswerYes(answer));
+            await promiseCheck;
+            if (isCorrect){
+                return userName;
+            } else {
+                if (nAttempts < maxAttempts){
+                    await speak(bot, 'Oh ok, let\'s try again. What is your name?');
+                } else {
+                    return '';
+                }
+                userName = '';
+            }
+        }
+    }
+}
+
+// function to execute the behaviour for
+// remembering the user's face
+async function rememberUserIdentity(msg){
+    console.log('call to rememberUserIdentity');
+
+    await speak(msg.bot, 'Ok, I will remember your identity');
+    await speak(msg.bot, 'What is your name?');
+    
+    let userName = await askName(msg.bot, 3);
+    if (userName === ''){
+        await speak(msg.bot, 'Sorry but I was not able to get your name');
+        return false;
+    }
+    await speak(msg.bot, 'Nice to meet you ' + userName);
+    await speak(msg.bot, 'Please, face the camera and wait');
+
+    // passing also the bot name to store the identity
+    // in separate variables
+    let outcome = await storeUserIdentity(userName, msg.bot);
+    if (outcome === true){
+        await speak(msg.bot, 'Done, I stored your identity');
+    } else {
+        await speak(msg.bot, 'Sorry, I was not able to detect your face');
+        return false;
+    }
+    return true;
+}
+
+// function to execute the behaviour of
+// recognising the user's identity
+async function recogniseUserIdentity(msg){
+    console.log('call to recogniseUserIdentity');
+    await speak(msg.bot, 'Ok, I will recognise you');
+    await speak(msg.bot, 'Please, face the camera and wait');
+
+    // Passing the bot name as parameter to
+    // recognise from 2 different variables
+    let identity = await recogniseFace(msg.bot);
+    if (identity === false){
+        await speak(msg.bot, 'Sorry, I am not able to recognise you');
+    } else {
+        await speak(msg.bot, 'I think you are ' + identity);
+    }
+}
+
+// function to execute the behaviour of
+// recognising the user's gender and age
+async function recogniseUserAgeAndGender(msg){
+    console.log('call to recogniseUserAgeAndGender');
+    await speak(msg.bot, 'Ok, I will recognise your age and gender');
+    await speak(msg.bot, 'Please, face the camera and wait');
+
+    let outcome = await recogniseAgeGender();
+    if (outcome === false){
+        await speak(msg.bot, 'Sorry, I am not able to recognise your age and gender');
+    } else {
+        await speak(msg.bot, 'I think you are a ' + outcome[1] + ' and about ' + outcome[0] + ' years old');
+    }
+}
+
+async function guessingGame(msg){
+    let docx = nlp(msg.content);
+    let whatToGuess = docx.match('Guess my [<what>*]').groups('what').text();
+    if (whatToGuess !== ''){
+        switch(whatToGuess){
+            case 'name':
+                recogniseUserIdentity(msg);
+                break;
+            case 'age':
+                recogniseUserAgeAndGender(msg);
+                break;
+            case 'age and gender':
+                recogniseUserAgeAndGender(msg);
+                break;
+            case 'gender':
+                recogniseUserAgeAndGender(msg);
+                break;
+            default:
+                await speak(msg.bot, 'Sorry, but I cannot guess your ' + whatToGuess);
+        }
+    }
+}
+
+// Added two new commands to recognise the user identity
+var botLogic = [
+    ["I (love|like) you", manageEmotionalState],
+    ["Remember me", rememberUserIdentity],
+    ["Recognise me", recogniseUserIdentity],
+    ["Who am I", recogniseUserIdentity],
+    ["Guess my [<what>*]", guessingGame]
+];
+
+// function to select the most
+// appropriate behaviour to execute
+function behaviourManager(msg){
+    for (let behaviour of botLogic){
+        let docx = nlp(msg.content);
+        if (docx.match(behaviour[0]).length > 0){
+            return behaviour[1];
+        }
+    }
+
+    // if we are here, it means we did not find
+    // any match for the command, so it is a general speech
+    // return false
+    return false;
+}
+
+// creating subscribers to the 'user_message' topic
+const picoChatListener = manager.subscribe(
+    'user_message',
+    function(message){
+        if (message.bot == 'pico'){
+            let behaviour = behaviourManager(message);
+            if (behaviour === false){
+                // general chat
+                let botSpeech = "I heard: " + message.content;
+                // execute the speech
+                speak(message.bot, botSpeech);
+            } else {
+                behaviour(message);
+            }
+        }
+    }
+)
+
+// creating a subscriber to the topic 'emotional_state_changed'
+const bitEmotionalStateListener = manager.subscribe(
+    'emotional_state_changed',
+    function(message){
+        if (message.bot == 'pico' && message.emotionalState == 'happy'){
+            // Bit gets jelous
+            let jelousSpeech = 'Hey! Talk to me!';
+            speak('bit', jelousSpeech);
+        }
+    }
+)
+
+const bitChatListener = manager.subscribe(
+    'user_message',
+    function(message){
+        if (message.bot == 'bit'){
+            let behaviour = behaviourManager(message);
+            if (behaviour === false){
+                // general chat
+                let botSpeech = "You said: " + message.content;
+                // execute the speech
+                speak(message.bot, botSpeech);
+            } else {
+                behaviour(message);
+            }
+        }
+    }
+)
+
+// creating a subscriber for the 'speech_event' topic
+const speechEventListener = manager.subscribe(
+    'speech_event',
+    function(message){
+        if (message.eventType == 'start'){
+            showTextBalloon(message.bot, message.speech);
+        } else {
+            hideTextBalloon(message.bot);
+        }
+    }
+)
+
+// Let's set a global variable to know if the
+// user was greeted or not
+var userGreeted = false;
+
+// we also need another variable to keep
+// track of the last detection timestamp
+// so we can compute the time between each
+// detection
+var lastDetectionTimestamp = 0;
+
+// let's create a subscriber to intercept
+// the messages on the topic
+// 'face_detection_complete'
+var engagementListener = manager.subscribe(
+    'face_detection_complete',
+    function(message){
+
+        // check if there are faces detected
+        var detections = message.detections;
+        if (detections.length > 0){
+            // There is a new detection
+            // Let's reset the last detection timestamp
+            // to the current time
+            lastDetectionTimestamp = Date.now();
+
+            // if it is the first time that we
+            // see the user, we greet them
+            if (!userGreeted){
+                speak('pico', 'Hello there!');
+                speak('bit', 'Nice to see you!');
+                // and then remember to set
+                // the global variable userGreeted to true
+                userGreeted = true;
+            }
+        } else {
+            // we do not only check if the face is not detected anymore
+            // but also the time elapsed from the last detection
+            // if it is more than 7 seconds we can be sure that the user
+            var timeElapsed = (Date.now() - lastDetectionTimestamp)/1000;
+            if (detections.length == 0 && userGreeted && timeElapsed > 7){
+                // we already greeted the user but
+                // there are no more detections so we
+                // say goodbye and disengage
+                // by setting userGreeted back to false
+                speak('pico', 'Goodbye');
+                speak('bit', 'See you later');
+                userGreeted = false;
+            }
+        }
+    }
+)
